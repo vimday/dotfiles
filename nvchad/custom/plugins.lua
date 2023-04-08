80448b19a608c67b7b8449a582326e8ad3593739
Crescent617 <lihuaru617@outlook.com>
2023-04-08 12:07:07 +0800

    temp rm noice.nvim

--- nvchad/custom/plugins.lua
+++ nvchad/custom/plugins.lua
@@ -611,45 +611,45 @@ local M = { -- utils
       vim.fn["mkdp#util#install"]()
     end,
   },
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("noice").setup {
-        -- you can enable a preset for easier configuration
-        presets = {
-          bottom_search = true, -- use a classic bottom cmdline for search
-          command_palette = true, -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false, -- add a border to hover docs and signature help
-        },
-        lsp = {
-          hover = { enabled = false },
-          progress = { enabled = false },
-          signature = { enabled = false },
-          messages = { view = "mini" },
-        },
-        popupmenu = { enabled = false },
-        -- messages = {
-        --   view = "mini",
-        -- },
-      }
-    end,
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      {
-        "rcarriga/nvim-notify",
-        config = function()
-          require("notify").setup { render = "compact" }
-        end,
-      },
-    },
-  },
+  -- {
+  --   "folke/noice.nvim",
+  --   event = "VeryLazy",
+  --   config = function()
+  --     require("noice").setup {
+  --       -- you can enable a preset for easier configuration
+  --       presets = {
+  --         bottom_search = true, -- use a classic bottom cmdline for search
+  --         command_palette = true, -- position the cmdline and popupmenu together
+  --         long_message_to_split = true, -- long messages will be sent to a split
+  --         inc_rename = false, -- enables an input dialog for inc-rename.nvim
+  --         lsp_doc_border = false, -- add a border to hover docs and signature help
+  --       },
+  --       lsp = {
+  --         hover = { enabled = false },
+  --         progress = { enabled = false },
+  --         signature = { enabled = false },
+  --         messages = { view = "mini" },
+  --       },
+  --       popupmenu = { enabled = false },
+  --       messages = {
+  --         view = "mini",
+  --       },
+  --     }
+  --   end,
+  --   dependencies = {
+  --     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+  --     "MunifTanjim/nui.nvim",
+  --     -- OPTIONAL:
+  --     --   `nvim-notify` is only needed, if you want to use the notification view.
+  --     --   If not available, we use `mini` as the fallback
+  --     {
+  --       "rcarriga/nvim-notify",
+  --       config = function()
+  --         require("notify").setup { render = "compact" }
+  --       end,
+  --     },
+  --   },
+  -- },
   { "AndrewRadev/bufferize.vim", event = "VeryLazy" },
   {
     "leoluz/nvim-dap-go",
