933f56e3d27cf69bd732b61c5778e335f3785001
Crescent617 <lihuaru617@outlook.com>
2023-07-09 11:15:02 +0800

    update brewlist

--- .shellrc.sh
+++ .shellrc.sh
@@ -52,6 +52,10 @@ fi
 
 # alias
 alias v=nvim
+if $(which podman >/dev/null 2>&1); then
+  alias docker=podman
+  alias lazydocker='DOCKER_HOST=unix:///run/user/1000/podman/podman.sock lazydocker'
+fi
 
 # --------------- APPs ----------------
 
