4085efec4ba33312a7c9be2522d26b3b280054f7
Crescent617 <lihuaru617@outlook.com>
2024-09-05 15:24:05 +0800

    add cursor-like

--- nvchad/lua/plugins/init.lua
+++ nvchad/lua/plugins/init.lua
@@ -21,7 +21,7 @@ return {
         lua = { "stylua" },
         python = { "isort", "black" },
         javascript = { { "prettierd", "prettier" } },
-        go = { "goimports", "gofmt" },
+        go = { "goimports", "gofmt", "golines" },
       },
       -- Set up format-on-save
       -- format_on_save = { timeout_ms = 500, lsp_fallback = true },
@@ -162,7 +162,16 @@ return {
       },
     },
   },
-  { "akinsho/git-conflict.nvim", version = "*", config = true, event = "BufRead" },
+  {
+    "akinsho/git-conflict.nvim",
+    version = "*",
+    config = function()
+      require("git-conflict").setup {
+        default_mappings = false,
+      }
+    end,
+    event = "BufRead",
+  },
   {
     "ggandor/leap.nvim",
     event = "BufRead",
@@ -468,7 +477,7 @@ return {
           -- "Classic", --# display results in the command-line  area
           -- "VirtualTextOk", --# display ok results as virtual text (multiline is shortened)
 
-          "VirtualText",             --# display results as virtual text
+          "VirtualText", --# display results as virtual text
           -- "TempFloatingWindow",      --# display results in a floating window
           "LongTempFloatingWindow", --# same as above, but only long results. To use with VirtualText[Ok/Err]
           -- "Terminal",                --# display results in a vertical split
@@ -772,7 +781,7 @@ return {
           vim.cmd "CopilotChatToggle"
         end,
         mode = "n",
-        desc = "AI",
+        desc = "CopilotChat",
       },
     },
     opts = {
@@ -836,4 +845,46 @@ return {
   --     })
   --   end,
   -- },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    -- lazy = false,
+    opts = {
+      provider = "copilot",
+    },
+    build = ":AvanteBuild", -- This is optional, recommended tho. Also note that this will block the startup for a bit since we are compiling bindings in Rust.
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to setup it properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
 }
