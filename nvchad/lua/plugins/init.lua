eb0ada5f1bfbcbad261687bb910dffa65d881eb2
Crescent617 <lihuaru617@outlook.com>
2024-09-26 13:12:36 +0800

    nvim: refine

--- nvchad/lua/plugins/init.lua
+++ nvchad/lua/plugins/init.lua
@@ -444,29 +444,6 @@ return {
     end,
   },
   {
-    "michaelb/sniprun",
-    -- enabled = false,
-    cmd = { "SnipRun", "SnipInfo" },
-    build = "bash ./install.sh",
-    config = function()
-      require("sniprun").setup {
-        repl_enable = { "Python3_original" },
-        display = {
-          -- "Classic", --# display results in the command-line  area
-          -- "VirtualTextOk", --# display ok results as virtual text (multiline is shortened)
-
-          "VirtualText", --# display results as virtual text
-          -- "TempFloatingWindow",      --# display results in a floating window
-          "LongTempFloatingWindow", --# same as above, but only long results. To use with VirtualText[Ok/Err]
-          -- "Terminal",                --# display results in a vertical split
-          -- "TerminalWithCode",        --# display results and code history in a vertical split
-          -- "NvimNotify",              --# display with the nvim-notify plugin
-          -- "Api"                      --# return output to a programming interface
-        },
-      }
-    end,
-  },
-  {
     "akinsho/toggleterm.nvim",
     version = "*",
     event = "VeryLazy",
