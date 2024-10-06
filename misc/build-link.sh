34404b76e395d57e06513ff9350eb7d99274c9d3
Crescent617 <lihuaru617@outlook.com>
2024-10-07 00:29:02 +0800

    add picom.conf

--- misc/build-link.sh
+++ misc/build-link.sh
@@ -11,25 +11,61 @@ backup() {
 link() {
   echo "link $1 to $2"
   if [[ -e $2 ]]; then
-    echo "file exists, make backup"
-    backup $2
+    echo "$2 exists, skip"
+    # backup $2
   elif [[ -L $2 ]]; then
-    echo "link exists, rm it"
-    rm $2
+    echo "$2 link exists, skip"
+    # rm $2
+  else
+    ln -s $1 $2
+    echo "linked"
   fi
-
-  ln -s $1 $2
 }
 
-link "$cur_dir/.tmux.conf" "$HOME/.tmux.conf"
-link "$cur_dir/.editorconfig" "$HOME/.editorconfig"
-link "$cur_dir/.vimrc" "$HOME/.vimrc"
-link "$cur_dir/.gitconfig" "$HOME/.gitconfig"
+link2dot_config() {
+  frm="$cur_dir/$1"
+  name=$(basename $frm)
+  link $frm "$HOME/.config/$name"
+}
+
+link2home() {
+  frm="$cur_dir/$1"
+  name=$(basename $frm)
+  link $frm "$HOME/$name"
+}
+
+# link for HOME
+link2homes=(
+  .tmux.conf
+  .editorconfig
+  .vimrc
+  .gitconfig
+  .todo.cfg
+  .condarc
+  .Xmodmap
+)
+
+for file in $link2homes; do
+  link2home $file
+done
 
 if [[ ! -d "$HOME/.config" ]]; then
   mkdir "$HOME/.config"
 fi
 
-link "$cur_dir/starship.toml" "$HOME/.config/starship.toml"
-link "$cur_dir/zellij" "$HOME/.config/zellij"
+# link for .config
+link2dot_configs=(
+  starship.toml
+  zellij
+  i3/i3
+  i3/i3blocks
+  picom.conf
+)
+
+for file in $link2dot_configs; do
+  link2dot_config $file
+done
+
+# special links
 link "$cur_dir/awesomewm" "$HOME/.config/awesome"
+link "$cur_dir/nvchad/lua" "$HOME/.config/nvim/lua"
