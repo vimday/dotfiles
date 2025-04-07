2b383860682d25094104cbba6eb07e0df99104e2
vimday <ms0x7f@gmail.com>
2025-04-07 13:45:32 +0800

    dddd

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,8 +1,8 @@
 -- ======================= CONSTANTS =======================
 -- If you are using mason.nvim, you can get the ts_plugin_path like this
 local mason_registry = require "mason-registry"
-local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
-  .. "/node_modules/@vue/language-server"
+-- local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
+--   .. "/node_modules/@vue/language-server"
 
 -- ======================= LSP CONFIGURATION =======================
 local servers = {
@@ -15,16 +15,16 @@ local servers = {
   gopls = {},
   ts_ls = {
     config = {
-      init_options = {
-        plugins = {
-          {
-            name = "@vue/typescript-plugin",
-            location = vue_language_server_path,
-            languages = { "vue" },
-          },
-        },
-      },
-      filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+      -- init_options = {
+      --   plugins = {
+      --     {
+      --       name = "@vue/typescript-plugin",
+      --       location = vue_language_server_path,
+      --       languages = { "vue" },
+      --     },
+      --   },
+      -- },
+      -- filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
     },
   },
   jsonls = {},
