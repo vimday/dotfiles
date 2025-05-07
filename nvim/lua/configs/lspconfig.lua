8ae9ea3420ee2ef91398e54c6e03999ca60c62b7
Crescent617 <lihuaru617@outlook.com>
2025-05-07 23:15:00 +0800

    nvim: update vue config

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,9 +1,3 @@
--- ======================= CONSTANTS =======================
--- If you are using mason.nvim, you can get the ts_plugin_path like this
-local mason_registry = require "mason-registry"
-local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
-  .. "/node_modules/@vue/language-server"
-
 -- ======================= LSP CONFIGURATION =======================
 local servers = {
   vimls = {},
@@ -19,7 +13,7 @@ local servers = {
         plugins = {
           {
             name = "@vue/typescript-plugin",
-            location = vue_language_server_path,
+            location = vim.fn.exepath "vue-language-server",
             languages = { "vue" },
           },
         },
