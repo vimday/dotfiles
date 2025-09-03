ef62ada2cff4d5c0733fa558543ad2acfa7196c6
Crescent617 <lihuaru617@outlook.com>
2025-09-03 13:58:54 +0800

    feat(nvim): new random func

--- .config/nvim/lua/custom/lib/rand.lua
+++ .config/nvim/lua/custom/lib/rand.lua
@@ -1,11 +1,23 @@
 local M = {}
 
+-- Return a random integer in [0, n)
+M.randn = function(n)
+  assert(n > 0, "n must be positive")
+  local bytes = vim.uv.random(4)
+  local val = 0
+  for i = 1, #bytes do
+    val = val * 256 + bytes:byte(i)
+  end
+
+  return (val % n)
+end
+
+-- Return a random element from an array
 M.pick = function(arr)
   if #arr == 0 then
     return
   end
-  local ns = vim.uv.hrtime() -- nvim >= 0.10
-  return arr[(ns % #arr) + 1]
+  return arr[M.randn(#arr) + 1]
 end
 
 return M
